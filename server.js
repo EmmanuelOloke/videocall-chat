afebf0720d6fdc2f530bdae39b22cb9ff929bf4f
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-04-19 15:24:05 +0100

    changed socket.io declaration to the newer one to avoid cors errors

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const socket = require('socket.io');
-const io = socket(server);
+// const socket = require('socket.io');
+// const io = socket(server);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+// Create a connection through socket.io
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id); // connect two user with their socket id
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded'); // disconnect when call is ended
+  });
+
+  socket.on('callUser', (data) => {
+    // when a call is initiated
+    io.to(data.userToCall).emit('callUser', {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on('answerCall', (data) => io.to(data.to).emit('callAccepted'), data.signal);
+});
 
 server.listen(3000, () => console.log('server is running on port 3000'));
