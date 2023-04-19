0380c6add01826db8e5bb4e7206434303f79cbe0
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-04-19 16:40:58 +0100

    decalred functions to call a user [callUser]; answer a call [answerCall]; and to leave a call [leaveCall]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { AssignmentIcon } from 'react-icons/md';
 import { useState, useEffect, useRef } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Peer from 'simple-peer';
-import io from 'socket.io-client';
+import io, { connect } from 'socket.io-client';
 import './App.css';
 
 const socket = io.connect('http://localhost:3000');
@@ -25,8 +25,82 @@ function App() {
   const connectionRef = useRef();
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    // Ask user permission to use webcam
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+      setStream(stream);
+      myVideo.current.srcObject = stream; // set the ref of myVideo to the stream coming in from the webcam
+    });
+
+    socket.on('me', (id) => {
+      // first socket.emit on the backend, which is going to emit socket.id
+      setMe(id); // set me to the emited socket.id from the backend
+    });
+
+    socket.on('callUser', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
   }, []);
+
+  const callUser = (id) => {
+    // funcitonality to call a user using simple peer library: a wrapper for WebRTC
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on('signal', (data) => {
+      // when a peer is created, emit callUser with the following data
+      socket.emit('callUser', {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name,
+      });
+    });
+
+    peer.on('stream', (stream) => {
+      userVideo.current.srcObject = stream; // get the other person's video and set it to stream
+    });
+
+    // add the callAccepted socket and set the peer signal
+    socket.on('callAccepted', (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+
+    // set the connectionRef to peer, so we can disable that once the call ends
+    connectionRef.current = peer;
+  };
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on('signal', (data) => {
+      socket.emit('answerCall', { signal: data, to: caller });
+    });
+
+    peer.on('stream', (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  };
 
   return (
     <div className="App">
