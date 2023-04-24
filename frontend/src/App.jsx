55abc3ecd4f5e92fd168c369e337860d605ebd82
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-04-24 16:04:16 +0100

    changed style colors and fixed some bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import { Button, IconButton, Input } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, IconButton, Input, Text, VStack } from '@chakra-ui/react';
 import { PhoneIcon, CopyIcon } from '@chakra-ui/icons';
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+// import global from 'global';
+// import * as process from 'process';
+// global.process = process;
+
 import Peer from 'simple-peer';
 import io from 'socket.io-client';
 import './App.css';
@@ -13,14 +18,14 @@ function App() {
   const [stream, setStream] = useState();
   const [receivingCall, setReceivingCall] = useState(false);
   const [caller, setCaller] = useState('');
-  const [callerSignal, setCallerSignal] = useState('');
+  const [callerSignal, setCallerSignal] = useState();
   const [callAccepted, setCallAccepted] = useState(false);
   const [idToCall, setIdToCall] = useState('');
   const [callEnded, setCallEnded] = useState(false);
   const [name, setName] = useState('');
 
-  const myVideo = useRef();
-  const userVideo = useRef();
+  const myVideo = useRef({});
+  const userVideo = useRef({});
   const connectionRef = useRef();
 
   useEffect(() => {
@@ -45,11 +50,16 @@ function App() {
 
   const callUser = (id) => {
     // funcitonality to call a user using simple peer library: a wrapper for WebRTC
+
+    console.log('gets here');
+
     const peer = new Peer({
       initiator: true,
       trickle: false,
       stream: stream,
     });
+
+    console.log(peer);
 
     peer.on('signal', (data) => {
       // when a peer is created, emit callUser with the following data
@@ -102,60 +112,96 @@ function App() {
   };
 
   return (
-    <>
-      <h1 style={{ textAlign: 'center', color: '#FFF' }}>TalentPlus Video Call</h1>
-      <div className="container">
-        <div className="video-container">
-          <div className="video">
-            {stream && (
-              <video playsInline muted ref={myVideo} autoPlay style={{ width: '18.75rem' }} />
-            )}
-          </div>
+    <VStack background="#008af3" height="auto" minHeight="100%" py={8}>
+      <Text fontSize="2rem" fontWeight="bold" textAlign="center" color="#FFF">
+        TalentPlus Video Call
+      </Text>
 
-          <div className="video">
-            {callAccepted && !callEnded ? (
-              <video playsInline ref={userVideo} autoPlay style={{ width: '18.75rem' }} />
-            ) : null}
-          </div>
-        </div>
-
-        <div className="myId">
-          <Input
-            size="lg"
-            variant="outline"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            mb={5}
-          />
-
-          <CopyToClipboard text={me} style={{ marginBottom: '2rem' }}>
-            <Button variant="contained" color="primary" leftIcon={<CopyIcon fontSize="large" />}>
-              Copy ID
-            </Button>
-          </CopyToClipboard>
-
-          <Input
-            id="filled-basic"
-            label="ID to call"
-            variant="filled"
-            value={idToCall}
-            onChange={(e) => setIdToCall(e.target.value)}
-          />
-
-          <div className="call-button">
-            {callAccepted && !callEnded ? (
-              <Button variant="contained" color="secondary" onClick={leaveCall}>
-                End Call
-              </Button>
+      <VStack gap={7}>
+        <Flex gap={2}>
+          <Box>
+            {stream ? (
+              <video
+                playsInline
+                muted
+                ref={myVideo}
+                autoPlay
+                style={{ width: '30rem', borderRadius: '10px' }}
+              />
             ) : (
-              <IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-                <PhoneIcon fontSize="large" />
-              </IconButton>
+              <Text fontSize="2rem" color="#FFF">
+                Loading...
+              </Text>
             )}
-            {idToCall}
-          </div>
-        </div>
+          </Box>
+
+          <Box>
+            {callAccepted && !callEnded ? (
+              <video playsInline ref={userVideo} autoPlay style={{ width: '30rem' }} />
+            ) : null}
+          </Box>
+        </Flex>
+
+        <Box
+          borderRadius={4}
+          background="#FFF"
+          width="25rem"
+          height="20rem"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <VStack width="15rem" gap={5}>
+            <Input
+              variant="filled"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <CopyToClipboard text={me} style={{ width: '15rem' }}>
+              <Button
+                variant="solid"
+                color="#FFF"
+                background="#008af3"
+                leftIcon={<CopyIcon />}
+                _hover={{ background: '#005da3' }}
+              >
+                Copy ID
+              </Button>
+            </CopyToClipboard>
+
+            <Input
+              placeholder="ID to call"
+              variant="filled"
+              value={idToCall}
+              onChange={(e) => setIdToCall(e.target.value)}
+            />
+
+            <Box>
+              {callAccepted && !callEnded ? (
+                <Button variant="outline" color="#FFF" background="#008af3" onClick={leaveCall}>
+                  End Call
+                </Button>
+              ) : (
+                <Button
+                  width="15rem"
+                  variant="outline"
+                  color="#008af3"
+                  colorScheme="#008af3"
+                  aria-label="call"
+                  onClick={() => callUser(idToCall)}
+                  size="lg"
+                  leftIcon={<PhoneIcon />}
+                  _hover={{ color: '#008af3', colorScheme: '#008af3' }}
+                >
+                  Call
+                </Button>
+              )}
+              {idToCall}
+            </Box>
+          </VStack>
+        </Box>
 
         <div>
           {receivingCall && !callAccepted ? (
@@ -167,8 +213,8 @@ function App() {
             </div>
           ) : null}
         </div>
-      </div>
-    </>
+      </VStack>
+    </VStack>
   );
 }
 
